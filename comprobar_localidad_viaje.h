26fa5cd9c8a1573b4c56b324917b5162338f06a6
Luisso03 <126898817+Luisso03@users.noreply.github.com>
2023-03-12 18:59:41 +0100

    Add files via upload

---
+++ comprobar_localidad_viaje.h
@@ -0,0 +1,6 @@
+#ifndef COMPROBAR_LOCALIDAD_VIAJE_H_INCLUDED
+#define COMPROBAR_LOCALIDAD_VIAJE_H_INCLUDED
+#include "comprobar_estado_viaje.h"
+void comprobar_mostar_viaje(int *)
+
+#endif // COMPROBAR_LOCALIDAD_VIAJE_H_INCLUDED

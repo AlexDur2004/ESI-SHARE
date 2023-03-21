5b6caff3bb9ce646044a287da686eba140c2e1c6
Alejandro Durán <aduranobregon@gmail.com>
2023-03-21 16:36:37 +0100

    Corregido error de fecha actual, se detectaba la fecha de hoy, como anterior a la actual. Además se ha creado una función para que pida solamente la fecha, para ser usada a la hora de reservar un viaje.

--- fecha.h
+++ fecha.h
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <time.h>
 
-void leer_fecha(char *, char *, char *);
+void leer_fecha(char *, char *, char *); //para leer fecha, hora inic, hora fin.
+void leer_dia(char *); //para leer fecha.
 
 #endif // FECHA_H_INCLUDED
